f7c740abd1da55ece0fba9e476388f52f06822dd
Edouard Barbier <edouarbarbier@hotmail.fr>
2016-01-12 22:36:13 +0000

    Initial Commit

---
+++ eb-network-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eb-network-app
+//
+//  Created by Edouard Barbier on 12/01/16.
+//  Copyright © 2016 Edouard Barbier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
